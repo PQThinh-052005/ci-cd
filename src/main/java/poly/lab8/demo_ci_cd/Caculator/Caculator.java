b5f2488ddc6bae9eaaf342b9147eccaefea69c87
PQThinh-052005 <phamquangthinh2152005@gmail.com>
2025-04-08 22:42:01 +0700

    fail

--- src/main/java/poly/lab8/demo_ci_cd/Caculator/Caculator.java
+++ src/main/java/poly/lab8/demo_ci_cd/Caculator/Caculator.java
@@ -3,7 +3,7 @@ package poly.lab8.demo_ci_cd.Caculator;
 
 public class Caculator implements ICaculator{
     public Integer sum(int a, int b){
-        return 0;
+        return a/b;
     }
     public Integer sub(int a, int b){
         return 0;
